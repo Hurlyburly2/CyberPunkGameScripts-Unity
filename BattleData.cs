f83fcdc9182475e1c33b6d8a2e332b70124dd999
Hurlyburly2 <dougdougmann@gmail.com>
2020-03-15 11:18:45 -0400

    character name, health, energy loaded into battle

---
+++ BattleData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleData : MonoBehaviour
+{
+    // Next to do: Load scene and don't destroy this!!!
+    CharacterData character;
+
+    private void Awake()
+    {
+        int count = FindObjectsOfType<BattleData>().Length;
+
+        if (count > 1)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetCharacterData(CharacterData characterToSet)
+    {
+        character = characterToSet;
+    }
+
+    public CharacterData GetCharacter()
+    {
+        return character;
+    }
+}
