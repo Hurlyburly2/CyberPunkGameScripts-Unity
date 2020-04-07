d273f08182d173429b31284bdb01dcd1de6097df
Hurlyburly2 <dougdougmann@gmail.com>
2020-04-07 19:45:59 -0400

    center cards in picker

--- Card/CardPicker/CardPicker.cs
+++ Card/CardPicker/CardPicker.cs
@@ -11,12 +11,14 @@ public class CardPicker : MonoBehaviour
 
     Image cardBackImage;
     float cardWidth;
+    float contentWidth;
 
     // state
     List<DummyCard> cardOptions = new List<DummyCard>();
 
     public void Initialize(List<Card> cards, int amountToPick)
     {
+        contentWidth = 0;
         gameObject.SetActive(true);
         SetCardSize();
 
@@ -26,17 +28,31 @@ public class CardPicker : MonoBehaviour
             CreateCardOption(card, counter);
             counter++;
         }
+        // compensate for extra space added with last card:
+        contentWidth -= cardWidth;
+
+        CenterCards();
     }
 
     private void CreateCardOption(Card card, int counter)
     {
-        float imageOffset = counter * cardWidth;
+        float imageOffset = cardWidth;
+        contentWidth += cardWidth;
         if (counter > 0)
         {
             imageOffset += cardWidth / 4;
+            contentWidth += cardWidth / 4;
         }
+        imageOffset *= counter;
         DummyCard newDummyCard = Instantiate(dummycard, new Vector2(transform.position.x + imageOffset, transform.position.y), Quaternion.identity);
         newDummyCard.transform.SetParent(cardHolder.transform);
+
+        cardOptions.Add(newDummyCard);
+    }
+
+    private void CenterCards()
+    {
+        cardHolder.transform.position = new Vector2(transform.position.x - contentWidth / 2, transform.position.y);
     }
 
     private void SetCardSize()
