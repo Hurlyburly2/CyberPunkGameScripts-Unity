eadb60b09034f0a4e73cb36041c1b04278e0aecd
Hurlyburly2 <dougdougmann@gmail.com>
2020-04-25 20:07:51 -0400

    some hack setup

---
+++ Hack/AllHackCards.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllHackCards : MonoBehaviour
+{
+    [SerializeField] HackCard[] allHackCards;
+
+    public HackCard GetCardById(int cardId)
+    {
+        return allHackCards[cardId];
+    }
+}
