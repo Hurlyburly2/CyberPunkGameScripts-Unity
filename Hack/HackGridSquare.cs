f0fa121e006fd35a9d188d69bfbb21ceb27e7a24
Hurlyburly2 <dougdougmann@gmail.com>
2020-05-01 21:08:22 -0400

    pseudocode for card checking

--- Hack/HackGridSquare.cs
+++ Hack/HackGridSquare.cs
@@ -68,9 +68,65 @@ public class HackGridSquare : MonoBehaviour
     {
         string[] connections = hackcard.GetConnectionsArray();
 
-        FindAndStoreAdjacentSquares();
+        // First, do it without rotation
+
+        // Check all four directions, at minimum all connections must be "ok" with one "connected"
+            // More than one "connected" is possible
+            // Cannot place with no "connected" UNLESS its the first card placed
+        // We check this for all four card orientations and keep track of the amount of ok orientations
+            // If more than one, we allow the user to rotate the card
+            // Disable card dragging during rotation
+        // If the card can only be placed in a rotated configuration, then rotate it and
+            // update all spike/circuit values
 
         return true;
+    }
+
+    private string IsLeftConnectionOk()
+    {
+        // Pass values here, don't use class variables, so we can
+            // reuse these checks with the temporary rotations
+        // For each direction, we check for a connection
+            // Match against a card with the same color
+                // return "connected"
+            // Match against a grid square with no card
+                // return "ok"
+        // AND we check for no conflicting connections
+        // Conflicting connections include:
+            // match against a card with a different color
+            // match against a card with no color in that direction
+                // return "mismatch"
+
+        return "ok";
+    }
+
+    private string IsAboveConnectionOk()
+    {
+        return "ok";
+    }
+
+    private string IsBelowConnectionOk()
+    {
+        return "ok";
+    }
+
+    private string IsRightConnectionOk()
+    {
+        return "ok";
+    }
+
+    private void RotateCardNinetyDegrees(int times = 1)
+    {
+        // Variables must be shifted one to the right (eg left => top, top => right, right => bottom, bottom => left)
+            // this must be done for circuits AND spikes
+        // We perform the rotation x times, 2 = 180 degrees, 3 = 270
+    }
+
+    private void CheckRotation(int times = 1)
+    {
+        // Store the variables for a card rotation and pass them into the connection ok
+            // methods
+        // This is done so we can test rotations without mutating the cards actual values
     }
 
     private void OnMouseExit()
