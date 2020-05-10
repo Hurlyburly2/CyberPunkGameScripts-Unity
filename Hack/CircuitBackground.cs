52da427246ab198bc51c441ccc9c2ba43cd1f3ff
Hurlyburly2 <dougdougmann@gmail.com>
2020-05-10 12:45:36 -0400

    background stuck to camera movement and scale

---
+++ Hack/CircuitBackground.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitBackground : MonoBehaviour
+{
+    public void UpdateBackgroundPos()
+    {
+        float cameraSize = Camera.main.orthographicSize;
+        transform.localScale = new Vector3(cameraSize, cameraSize, cameraSize);
+        transform.position = new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.y);
+    }
+}
