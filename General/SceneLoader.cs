f9e81775e7585a18855ae4620c76c09066cbc2b2
Hurlyburly2 <dougdougmann@gmail.com>
2020-06-13 20:49:59 -0400

    beginning of map, character portraits load correctly

--- General/SceneLoader.cs
+++ General/SceneLoader.cs
@@ -9,14 +9,17 @@ public class SceneLoader : MonoBehaviour
     int currentScene;
     BattleData currentBattle;
     HackBattleData currentHack;
+    MapData currentMap;
 
     // config
     [SerializeField] BattleData battleData;
     [SerializeField] HackBattleData hackBattleData;
+    [SerializeField] MapData mapData;
 
     // Scene names
     [SerializeField] string battleSceneName = "Battle";
     [SerializeField] string hackSceneName = "Hack";
+    [SerializeField] string mapSceneName = "Map";
 
     // Character/h@cker
     CharacterData currentRunner;
@@ -37,12 +40,38 @@ public class SceneLoader : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+
+        int mapCount = FindObjectsOfType<MapData>().Length;
+        if (mapCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
     // Start is called before the first frame update
     void Start()
     {
         currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void LoadMap(string mapType)
+    {
+        currentMap = Instantiate(mapData);
+        currentMap.SetCharacterData(currentRunner, currentHacker, mapType);
+
+        SceneManager.LoadScene(mapSceneName);
+
+        StartCoroutine(WaitForMapLoad(mapSceneName));
+    }
+
+    private IEnumerator WaitForMapLoad(string mapName)
+    {
+        while (SceneManager.GetActiveScene().name != mapSceneName)
+        {
+            yield return null;
+        }
+        currentMap.SetUpMap();
     }
 
     public void LoadBattle()
@@ -76,6 +105,13 @@ public class SceneLoader : MonoBehaviour
         currentRunner = TestData.SetTestCharacterOne();
         currentHacker = TestData.SetTestHackerOne();
         LoadHack();
+    }
+
+    public void LoadMapTestOne()
+    {
+        currentRunner = TestData.SetTestCharacterOne();
+        currentHacker = TestData.SetTestHackerOne();
+        LoadMap("city");
     }
 
     public void LoadHack()
---
+++ Map/MapData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour
+{
+    // config
+    CharacterData runner;
+    HackerData hacker;
+    string mapType;
+        // possible: "city"
+
+    private void Awake()
+    {
+        int count = FindObjectsOfType<MapData>().Length;
+
+        if (count > 1)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetUpMap()
+    {
+        PlayerPortrait[] portraits = FindObjectsOfType<PlayerPortrait>();
+        foreach (PlayerPortrait portrait in portraits)
+        {
+            if (portrait.name == "RunnerPortrait")
+            {
+                portrait.SetPortrait(runner.GetRunnerName());
+            } else if (portrait.name == "HackerPortrait")
+            {
+                portrait.SetPortrait(hacker.GetName());
+            }
+        }
+    }
+
+    public void SetCharacterData(CharacterData characterToSet, HackerData hackerToSet, string newMapType)
+    {
+        runner = characterToSet;
+        hacker = hackerToSet;
+        mapType = newMapType;
+    }
+}
