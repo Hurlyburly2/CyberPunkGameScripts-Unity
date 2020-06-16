c5886e7cd658421e95a928e9c53148f5446b4265
Hurlyburly2 <dougdougmann@gmail.com>
2020-06-16 18:14:56 -0400

    map squares spawn, count slightly inaccurate due to not controlling inside lower levels

--- Map/MapGrid.cs
+++ Map/MapGrid.cs
@@ -4,33 +4,87 @@ using UnityEngine;
 
 public class MapGrid : MonoBehaviour
 {
+    //config
     [SerializeField] MapSquareRow[] rows;
     int startingRow = 0;
     int startingSquare = 9;
     MapSquareImageHolder mapSquareImageHolder;
+    int percentChanceForSpawn = 10;
+
+    // state
+    List<MapSquareRow> activeRows;
 
     public void SetupGrid(string mapType, int mapSize)
     {
+        activeRows = new List<MapSquareRow>();
         mapSquareImageHolder = FindObjectOfType<MapSquareImageHolder>();
         mapSquareImageHolder.Initialize(mapType);
 
         SetupSquareLevels();
-        SetupMap(mapType, mapSize);
+        SetupMap(mapType, mapSize, GetVerticalModifier(mapType));
     }
 
-    private void SetupMap(string mapType, int mapSize)
+    private void SetupMap(string mapType, int mapSize, int mapSquareChanceForVerticalModifier)
     {
+        // initialize starting square in first row
         int activeSquares = 1;
-        List<int> activeRows = new List<int>();
+        activeRows.Add(rows[startingRow]);
+        rows[startingRow].InitializeFirstSquare(startingSquare, mapSquareImageHolder.GetSquareImage());
 
-        activeRows.Add(0);
-        rows[startingRow].GetSquareByRowPosition(startingSquare).InitializeSquare(mapSquareImageHolder.GetSquareImage());
-
+        // then start spawning the rest
+        int currentRow = startingRow;
+        ConsolidateNewActiveSquaresInAllRows();
         while (activeSquares < mapSize)
         {
+            // go through active rows and spawn squares around each active square
+            for (int i = activeRows.Count - 1; i >= 0; i--)
+            {
+                int amountOfNewSquares = activeRows[i].AttemptToSpawnSquares(percentChanceForSpawn, mapSize - activeSquares, mapSquareChanceForVerticalModifier);
+                activeSquares += amountOfNewSquares;
+            }
 
-            activeSquares++;
+            ConsolidateNewActiveSquaresInAllRows();
+
+            // then look for active rows and add them to the active rows list, setting them up for spawns as well
+            foreach (MapSquareRow row in rows)
+            {
+                if (row.ContainsActiveSquares() && !activeRows.Contains(row))
+                {
+                    activeRows.Add(row);
+                }
+            }
         }
+        int activeSquareAccurateCount = 0;
+        foreach(MapSquareRow row in rows)
+        {
+            activeSquareAccurateCount += row.CountActiveSquares();
+        }
+        Debug.Log("We counted " + activeSquareAccurateCount + " squares.");
+        Debug.Log("it thinks we made " + activeSquares + " squares.");
+    }
+
+    private int GetVerticalModifier(string mapType)
+    {
+        switch (mapType)
+        {
+            case "city":
+                return 35;
+            default:
+                return 35;
+        }
+    }
+
+    public void ConsolidateNewActiveSquaresInAllRows()
+    {
+        foreach (MapSquareRow row in rows)
+        {
+            row.ConsolidateNewActiveSquares();
+        }
+    }
+
+    public MapSquareRow GetRowByNumber(int rowNumber)
+    {
+        return rows[rowNumber];
     }
 
     private void SetupSquareLevels()
--- Map/MapSquare.cs
+++ Map/MapSquare.cs
@@ -4,12 +4,65 @@ using UnityEngine;
 
 public class MapSquare : MonoBehaviour
 {
+    //config
     [SerializeField] int rowPosition; // 0-19 position in row
     [SerializeField] MapSquareRow parentRow;
+    int minSquareRow = 0;
+    int maxSquareRow = 19;
+    int minSquareColumn = 0;
+    int maxSquareColumn = 19;
+
+    //state
+    bool isActive;
+
+    public List<MapSquare> GetAdjacentSquares()
+    {
+        List<MapSquare> adjacentSquares = new List<MapSquare>();
+
+        if (rowPosition > minSquareRow)
+        {
+            adjacentSquares.Add(parentRow.GetSquareByRowPosition(rowPosition - 1));
+        }
+        if (rowPosition < maxSquareRow)
+        {
+            adjacentSquares.Add(parentRow.GetSquareByRowPosition(rowPosition + 1));
+        }
+        if (parentRow.GetRowNumber() > minSquareColumn)
+        {
+            adjacentSquares.Add(parentRow.GetMapGrid().GetRowByNumber(parentRow.GetRowNumber() - 1).GetSquareByRowPosition(rowPosition));
+        }
+        if (parentRow.GetRowNumber() < maxSquareColumn)
+        {
+            adjacentSquares.Add(parentRow.GetMapGrid().GetRowByNumber(parentRow.GetRowNumber() + 1).GetSquareByRowPosition(rowPosition));
+        }
+
+        return adjacentSquares;
+    }
+
+    public MapSquare GetUpSquare()
+    {
+        if (parentRow.GetRowNumber() < maxSquareColumn)
+        {
+            return parentRow.GetMapGrid().GetRowByNumber(parentRow.GetRowNumber() + 1).GetSquareByRowPosition(rowPosition);
+        }
+        return null;
+    }
+
+    private void Start()
+    {
+        isActive = false;
+    }
 
     public void InitializeSquare(Sprite newImage)
     {
+        isActive = true;
+        parentRow.AddInitializedSquareToList(this);
         GetComponent<SpriteRenderer>().sprite = newImage;
+    }
+
+    public bool IsActive()
+    {
+        return isActive;
     }
 
     private void OnMouseUp()
