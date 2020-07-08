2f1583f06610c92f18d8e6dcd42126fd8a42d315
Hurlyburly2 <dougdougmann@gmail.com>
2020-07-07 21:28:21 -0400

    text is filled in to goal window

--- Map/UI/GoalWindow.cs
+++ Map/UI/GoalWindow.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GoalWindow : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI descriptionText;
+
     public void OpenGoalWindow()
     {
         gameObject.SetActive(true);
+        descriptionText.text = GenerateDescriptionText(FindObjectOfType<MapGrid>().GetMapType());
         FindObjectOfType<MapConfig>().SetIsAMenuOpen(true);
+    }
+
+    private string GenerateDescriptionText(string mapType)
+    {
+        switch (mapType)
+        {
+            case "slums":
+                return "You have obtained the item. Flavor & Story text will go here. Enemies are on high alert. Hurry to the extraction point!";
+        }
+        return "";
     }
 
     public void CloseGoalWindow()
