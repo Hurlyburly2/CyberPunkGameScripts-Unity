3de117af8a1eb436206f476517211ef8af76b9ad
Hurlyburly2 <dougdougmann@gmail.com>
2020-08-18 08:52:40 -0400

    proof of concept for generating scrollable inventory list

---
+++ HubWorld/Menus/InventoryListControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryListControl : MonoBehaviour
+{
+    [SerializeField] GameObject buttonTemplate;
+
+    private void Start()
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
+
+            button.GetComponent<InventoryListItem>().SetText("Button #" + i);
+
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
+        }
+    }
+}
---
+++ HubWorld/Menus/InventoryListItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryListItem : MonoBehaviour
+{
+    [SerializeField] Text myText;
+
+    public void SetText(string textString)
+    {
+        myText.text = textString;
+    }
+
+    public void ClickButton()
+    {
+
+    }
+}
