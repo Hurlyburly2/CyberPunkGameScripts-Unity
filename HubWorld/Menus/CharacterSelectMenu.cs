7566efffbf1113f2563144f20b7044ce49805b81
Hurlyburly2 <dougdougmann@gmail.com>
2020-09-03 21:28:13 -0400

    parts of character select menu done

---
+++ HubWorld/Menus/CharacterSelectMenu.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectMenu : MonoBehaviour
+{
+    [SerializeField] LoadoutMenu loadoutMenu;
+    Item.HackerRunner hackerOrRunner;
+
+    [SerializeField] Sprite selectHackerSprite;
+    [SerializeField] Sprite selectRunnerSprite;
+    [SerializeField] Image headingTextImage;
+    [SerializeField] Image currentSelectionPortrait;
+
+    HackerData currentHacker;
+    HackerData shownHacker;
+    CharacterData currentRunner;
+    CharacterData shownRunner;
+
+    public void SetupCharacterSelectMenu(Item.HackerRunner newHackerOrRunner)
+    {
+        hackerOrRunner = newHackerOrRunner;
+        PlayerData playerData = FindObjectOfType<PlayerData>();
+
+        switch (hackerOrRunner)
+        {
+            case Item.HackerRunner.Runner:
+                headingTextImage.sprite = selectRunnerSprite;
+                currentRunner = playerData.GetCurrentRunner();
+                shownRunner = currentRunner;
+                break;
+            case Item.HackerRunner.Hacker:
+                headingTextImage.sprite = selectHackerSprite;
+                currentHacker = playerData.GetCurrentHacker();
+                shownHacker = currentHacker;
+                break;
+        }
+        LoadCharacterPortrait();
+    }
+
+    private void LoadCharacterPortrait()
+    {
+        switch (hackerOrRunner)
+        {
+            case Item.HackerRunner.Runner:
+                currentSelectionPortrait.sprite = Resources.Load<Sprite>("Characters/Runner" + shownRunner.getId() + "-Full");
+                break;
+            case Item.HackerRunner.Hacker:
+                currentSelectionPortrait.sprite = Resources.Load<Sprite>("Characters/Hacker" + shownHacker.GetId() + "-Full");
+                break;
+        }
+    }
+
+    public void ConfirmSelectBtnPress()
+    {
+        // TODO CHARACTER SELECT STUFF IN HERE....
+        CloseCharacterSelectMenu();
+    }
+
+    public void CloseCharacterSelectMenu()
+    {
+        loadoutMenu.SetupLoadoutMenu();
+        gameObject.SetActive(false);
+    }
+}
---
+++ HubWorld/Menus/ShopMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopMenu : MonoBehaviour
+{
+    public void SetupShopMenu()
+    {
+        Debug.Log("Setup Shop Menu");
+    }
+
+    public void CloseShopMenu()
+    {
+        gameObject.SetActive(false);
+    }
+}
