6f1e86a7f055ba9570df3f854272e124d9855a94
Hurlyburly2 <dougdougmann@gmail.com>
2020-09-06 07:26:43 -0400

    loadout eq menu opens/closes and loads hacker/runner as appropriate

---
+++ HubWorld/Menus/LoadoutEquipmentMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadoutEquipmentMenu : MonoBehaviour
+{
+    Item.HackerRunner hackerOrRunner;
+
+    CharacterData runner;
+    HackerData hacker;
+
+    private void DoSetup()
+    {
+
+    }
+
+    public void SetupLoadoutEquipmentMenu(CharacterData newRunner)
+    {
+        runner = newRunner;
+        hackerOrRunner = Item.HackerRunner.Runner;
+        DoSetup();
+    }
+
+    public void SetupLoadoutEquipmentMenu(HackerData newHacker)
+    {
+        hacker = newHacker;
+        hackerOrRunner = Item.HackerRunner.Hacker;
+        DoSetup();
+    }
+
+    public void CloseLoadoutEquipmentMenu()
+    {
+        gameObject.SetActive(false);
+    }
+}
