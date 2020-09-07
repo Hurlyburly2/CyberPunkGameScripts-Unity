9708745b6f56cc8c87968be5e2cb8735cdbf1c20
Hurlyburly2 <dougdougmann@gmail.com>
2020-09-07 19:54:37 -0400

    hooked up inventory list

--- HubWorld/Menus/LoadoutEquipmentMenu.cs
+++ HubWorld/Menus/LoadoutEquipmentMenu.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LoadoutEquipmentMenu : MonoBehaviour
 {
+    // Runner stuff
     [SerializeField] GameObject runnerContext;
+    [SerializeField] List<LoadoutSlotBtn> runnerLoadoutSlotBtns;
+    [SerializeField] TextMeshProUGUI runnerName;
+    [SerializeField] TextMeshProUGUI runnerDescription;
+    [SerializeField] InventoryList inventoryList;
+
+    // Hacker Stuff
     [SerializeField] GameObject hackerContext;
-    [SerializeField] List<LoadoutSlotBtn> loadoutSlotBtns;
 
     Item.HackerRunner hackerOrRunner;
 
     CharacterData runner;
     HackerData hacker;
+    InventoryMenu.InventoryFields[] fields = { InventoryMenu.InventoryFields.Name, InventoryMenu.InventoryFields.Type, InventoryMenu.InventoryFields.Lvl };
 
     private void DoSetup()
     {
@@ -20,21 +28,39 @@ public class LoadoutEquipmentMenu : MonoBehaviour
             case Item.HackerRunner.Runner:
                 hackerContext.SetActive(false);
                 runnerContext.SetActive(true);
+                runnerName.text = runner.GetRunnerName();
+                runnerDescription.text = runner.GetBio();
                 break;
             case Item.HackerRunner.Hacker:
                 runnerContext.SetActive(false);
                 hackerContext.SetActive(true);
+                // setup hacker name
+                // setup hacker description (if necessary)
                 break;
         }
-        foreach (LoadoutSlotBtn button in loadoutSlotBtns)
+        foreach (LoadoutSlotBtn button in runnerLoadoutSlotBtns)
         {
             button.SetupButton();
         }
+        SetupInventoryList();
+    }
+
+    private void SetupInventoryList()
+    {
+        inventoryList.DestroyListItems();
+
+        List<Item> items = FindObjectOfType<PlayerData>().GetPlayerItems();
+        //List<Item> filteredItems = new List<Item>();
+
+        // Filter the items based on selection up above...
+
+        //inventoryList.SetupInventoryList(fields, filteredItems);
+        inventoryList.SetupInventoryList(fields, items);
     }
 
     public void HandlePressedSlotButton(Item.ItemTypes itemTypeOnButton, Loadout.LeftOrRight leftOrRight)
     {
-        foreach (LoadoutSlotBtn button in loadoutSlotBtns)
+        foreach (LoadoutSlotBtn button in runnerLoadoutSlotBtns)
         {
             if (itemTypeOnButton != button.GetItemType() || leftOrRight != button.GetLeftOrRight())
             {
