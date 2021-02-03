6349b58640104e6bbef355098b086b196c66d3c0
Hurlyburly2 <dougdougmann@gmail.com>
2021-02-02 21:38:28 -0500

    started adding battle sfx

---
+++ Battle/BattleSFX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSFX : MonoBehaviour
+{
+    [SerializeField] BattleSoundObject battleSoundObject;
+
+    [SerializeField] List<AudioClip> drawCardSounds;
+
+    int soundsCurrentlyPlaying = 0;
+    public enum BattleSoundEffect
+    {
+        DrawCard
+    };
+
+    public void PlayMapSoundSFX(BattleSoundEffect soundEffect)
+    {
+        if (soundsCurrentlyPlaying < 15)
+        {
+            switch (soundEffect)
+            {
+                case BattleSoundEffect.DrawCard:
+                    int randomIndex = Random.Range(0, drawCardSounds.Count);
+                    PlaySound(drawCardSounds[randomIndex]);
+                    break;
+            }
+        }
+    }
+
+    private void PlaySound(AudioClip soundToPlay, bool loop = false)
+    {
+        // By default, play without a loop
+        BattleSoundObject newSound = Instantiate(battleSoundObject);
+        soundsCurrentlyPlaying++;
+        newSound.PlaySound(soundToPlay, loop, this);
+    }
+
+    public void NotifyOfSoundCompletion()
+    {
+        soundsCurrentlyPlaying--;
+    }
+}
---
+++ Battle/BattleSoundObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSoundObject : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+    bool startedPlaying;
+    BattleSFX parent;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void PlaySound(AudioClip audioClip, bool loop, BattleSFX newParent)
+    {
+        parent = newParent;
+        audioSource.clip = audioClip;
+        audioSource.loop = loop;
+        audioSource.Play();
+        startedPlaying = true;
+    }
+
+    private void Update()
+    {
+        if (startedPlaying && !audioSource.isPlaying)
+        {
+            parent.NotifyOfSoundCompletion();
+            Destroy(gameObject);
+        }
+    }
+}
